9cf82c54a9f4b1365af0e81fc7d6ef508291b664
Yoana <yoanabarbie@icloud.com>
2024-07-26 20:51:14 +0200

    feat: added featured services on home page

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -9,17 +9,25 @@ import artists from "/images/artists.jpg"
 export const Home = () => {
   return (
     <>
-    <h1> Noble Art Studios </h1>
-    <img src={studio} className="imgHome"/>
-    <h2> Much more than a tattoo studio </h2>
-    <div className='left'>
-      <img src={fine_line} className='featured'/>
-      <img src={removal} />
-    </div>
-    <div className='right'>
-      <img src={piercing} />
-      <img src={artists} />
-    </div>
+      <h1> Noble Art Studios </h1>
+      <img src={studio} className="imgHome" alt="Studio"/>
+      <h2> Much more than a tattoo studio </h2>
+      
+      <div className="main-wrapper">
+        
+        <div className='left'>
+        <img src={fine_line} className='featured' alt="Fine Line"/>
+        <img src={piercing} className='featured' alt="Piercing"/>
+        </div>
+        
+        <div className='right'>
+        <img src={removal} className='featured' alt="Removal"/>
+          <img src={artists} className='featured' alt="Artists"/>
+        </div>
+
+      </div>
+
+      <h2>It's not a tattoo, it's art on your skin</h2>
     </>
   )
 }
