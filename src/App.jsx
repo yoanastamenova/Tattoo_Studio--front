c0f568850c3a5402e51d4085d81900955762113c
Yoana <yoanabarbie@icloud.com>
2024-07-27 18:52:08 +0200

    feat: tailwind config imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import "./App.css";
+import "./index.css";
 import { Header } from "./components/Header/Header.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 import { Body } from "./views/Body/Body.jsx";
+
 
 function App() {
     return (
